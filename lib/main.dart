2efc0a3f29c7c713b0d7910a90e6d442d8b5402b
Hemant Patel <hemantpatel@192.168.1.5>
2023-09-16 12:01:14 +0530

    remove block pattern folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,175 @@
 import 'package:flutter/material.dart';
-import 'package:uno_project/screens/post_list.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(MyApp());
+  runApp(JsonPlaceholderApp());
 }
 
-class MyApp extends StatelessWidget {
+class JsonPlaceholderApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter JSON Example',
-      home: PostList(),
+      title: 'JsonPlaceholder App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PostListScreen(),
+    );
+  }
+}
+
+class PostListScreen extends StatefulWidget {
+  @override
+  _PostListScreenState createState() => _PostListScreenState();
+}
+
+class _PostListScreenState extends State<PostListScreen> {
+  List<dynamic> posts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+  }
+
+  Future<void> fetchPosts() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    if (response.statusCode == 200) {
+      setState(() {
+        posts = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load posts');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Post List'),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 600) {
+            // Use a GridView for larger screens
+            return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                final post = posts[index];
+                return Card(
+                  margin: EdgeInsets.all(8),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              CommentListScreen(postId: post['id']),
+                        ),
+                      );
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            post['title'],
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(post['body']),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            // Use a ListView for smaller screens
+            return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                final post = posts[index];
+                return ListTile(
+                  title: Text(post['title']),
+                  subtitle: Text(post['body']),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            CommentListScreen(postId: post['id']),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class CommentListScreen extends StatefulWidget {
+  final int postId;
+
+  CommentListScreen({required this.postId});
+
+  @override
+  _CommentListScreenState createState() => _CommentListScreenState();
+}
+
+class _CommentListScreenState extends State<CommentListScreen> {
+  List<dynamic> comments = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchComments();
+  }
+
+  Future<void> fetchComments() async {
+    final response = await http.get(
+        Uri.parse('https://jsonplaceholder.typicode.com/comments?postId=${widget.postId}'));
+    if (response.statusCode == 200) {
+      setState(() {
+        comments = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load comments');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Comments'),
+      ),
+      body: ListView.builder(
+        itemCount: comments.length,
+        itemBuilder: (context, index) {
+          final comment = comments[index];
+          return ListTile(
+            title: Text(comment['name']),
+            subtitle: Text(comment['email']),
+            onTap: () {
+              // Handle tapping on individual comments if needed.
+            },
+          );
+        },
+      ),
     );
   }
 }
